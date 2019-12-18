5a47695b84747e4111f23cf8b7d86e2569a69089
caohouhong <229336009@qq.com>
2019-12-18 15:31:55 +0800

    老的报错，现在是新的

---
+++ NSArray+MCLog.h
@@ -0,0 +1,19 @@
+//
+//  NSArray+MCLog.h
+//  Transfer
+//
+//  Created by caohouhong on 2019/12/18.
+//  Copyright © 2019 chh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSArray (MCLog)
+
+@end
+@interface NSDictionary (MCLog)
+
+@end
+NS_ASSUME_NONNULL_END
